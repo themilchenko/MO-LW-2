bf3d7247aaa2338c8f915e380a70cd9890287541
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-10-16 13:04:03 +0300

    Add files via upload

---
+++ Symplex.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Symplex
+{
+    /*матрица преобразований*/
+    std::vector<std::vector<double>> table_;
+    std::vector<double> goal_func;
+
+    /*резрешающие таблица и строка*/
+    int permissive_str_;
+    int permissive_column_;
+
+    /*вектора переменных для таблицы*/
+    std::vector<std::string> basis_;
+    std::vector<std::string> free_;
+
+public:
+    /*конструктор для сборки таблицы из файла*/
+    Symplex(std::ifstream& input);
+
+    /*проверка на оптимальность*/
+    bool is_optimal() const;
+
+    /*печать таблицы*/
+    void print() const;
+
+    /*поиск разрешающей колнки и сроки*/
+    int find_column() const;
+    int find_str() const;
+    int find_min() const;
+
+    /*одна итерация преобразования*/
+    void do_step();
+
+    /*геттер оптимального решения*/
+    double get_solution() const;
+
+    void do_examination() const;
+
+    void make_dual();
+
+    ~Symplex()
+    {
+        for (auto& i : table_)
+            i.clear();
+        table_.clear();
+        basis_.clear();
+        free_.clear();
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,54 @@
+// IU8-34, Milchenko Ivan, 14 Variant
+// F = cx -> max
+// Ax <= b
+
+#include "Symplex.h"
+
+int main()
+{
+    std::string path = "/Users/ivanmilchenko/MO-lab-01/input.txt";
+    std::ifstream input(path);
+    if (!input)
+        throw std::runtime_error("Unable to open file " + path);
+
+    Symplex problem(input);
+
+    std::ifstream input1(path);
+    Symplex dual_problem(input1);
+
+    std::cout << "THIS IS YOUR PROBLEM IN SYMPLEX TABLE:\n";
+    problem.print();
+
+    size_t iterations = 0;
+
+    while (problem.is_optimal())
+    {
+        ++iterations;
+        std::cout << "THE " << iterations << " STEP:\n";
+        problem.do_step();
+        problem.print();
+    }
+
+    std::cout << "THE SOLUTION IS " << problem.get_solution() << std::endl;
+    problem.do_examination();
+    std::cout << std::endl;
+
+    // DUAL PROBLEM
+    std::cout << "DUAL PROBLEM:\n";
+    dual_problem.make_dual();
+    dual_problem.print();
+
+    do
+    {
+        ++iterations;
+        std::cout << "THE " << iterations << " STEP:\n";
+        dual_problem.do_step();
+        dual_problem.print();
+    } while (dual_problem.is_optimal());
+
+    std::cout << "THE SOLUTION IS " << -dual_problem.get_solution() << std::endl;
+    dual_problem.do_examination();
+    std::cout << std::endl;
+
+    return 0;
+}
